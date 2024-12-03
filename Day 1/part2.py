f5a3c2bd879bb4f565f00c2a39f3c7e159350060
Kiiya <abderrahmanlabiod@iessantiagohernandez.com>
2024-12-03 23:55:17 +0100

    AoC 2024 - Day 1 - Part 2

---
+++ Day 1/part2.py
@@ -0,0 +1,27 @@
+file = 'C:/Users/Kiiya/Documents/GitHub/aoc2024/Day 1/input.txt'
+
+leftValues = []
+rightValues = []
+
+## Read the input file containing left and right values
+def initializeInput(file):
+    with open(file, 'r') as f:
+        values = f.read().splitlines()
+        for val in values:
+            numbers = val.split("   ")
+            left = numbers[0]
+            right = numbers[1]
+            leftValues.append(int(left))
+            rightValues.append(int(right))
+
+def similarityScore():
+    score = 0
+    for leftValue in leftValues:
+        times = 0
+        for rightValue in rightValues: 
+            if leftValue == rightValue: times += 1
+        score = score + leftValue*times
+    return score
+
+initializeInput(file)
+print(similarityScore())
