3d68b57c63e2a79791020f0ee88078aab4636bc1
Kiiya <abderrahmanlabiod@iessantiagohernandez.com>
2024-12-04 17:16:24 +0100

    Completed Day 2 part 1

--- Day 2/part1.py
+++ Day 2/part1.py
@@ -24,24 +24,29 @@ def checkSafeReports():
     for i in range(0, len(reports)):
         # print("Scanning repot Nº" + str(i))
         report = list(reports[i])
-        firstReportValue = report[0]
-
         difference = report[0] - report[1]
 
         decreasing = False
         if difference > 0: decreasing = True
-        # print(str(decreasing) + " " + str(report))
+        if difference == 0: continue 
 
-        safe = False
-        for j in range(2, len(report)):
-            if decreasing and firstReportValue >= report[j]: break
-            elif (not decreasing) and firstReportValue <= report[j]: break
-            safe = True
+        safe = True
+        for j in range(1, len(report)):
+            if decreasing and report[j-1] <= report[j]: 
+                safe = False
+                #print("Decreasing " + str(report) + " report has a greater value which is: " + str(report[j]))
+                break
+            elif (not decreasing) and report[j-1] >= report[j]: 
+                safe = False
+                #print("Increasing " + str(report) + " report has a smaller value which is: " + str(report[j]))
+                break
         if safe: tempSafeReports.append(report)
         
     for i in range(0, len(tempSafeReports)):
         report = tempSafeReports[i]
-        if isSafe(report): safeReports += 1
+        if isSafe(report): 
+            print(report)
+            safeReports += 1
     return safeReports
 
 initializeInput(file) 
